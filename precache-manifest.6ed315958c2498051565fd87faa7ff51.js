d9bb69876c1febac8cf05fc03d50c42fd4e4804f
Shan Li Nio <shanlinio1992@gmail.com>
2019-08-12 21:46:01 +0200

    Updates

---
+++ precache-manifest.6ed315958c2498051565fd87faa7ff51.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7896ef3e739953df4125ca8b3f9997bb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1c44bff77508b968fc5",
+    "url": "/portfolio/static/css/main.a97fff95.chunk.css"
+  },
+  {
+    "revision": "2ee3b91efe47ad0dbe8c",
+    "url": "/portfolio/static/js/2.2bb5dc31.chunk.js"
+  },
+  {
+    "revision": "c1c44bff77508b968fc5",
+    "url": "/portfolio/static/js/main.50493bc3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ad758c8b4b7cdd9ae8b6ec47d0c9a98e",
+    "url": "/portfolio/static/media/animal1.ad758c8b.JPG"
+  },
+  {
+    "revision": "03ae012bced232b0dbb7e3a97e0761ee",
+    "url": "/portfolio/static/media/city1.03ae012b.JPG"
+  },
+  {
+    "revision": "51e0715dbe9c6b32061849f75176e073",
+    "url": "/portfolio/static/media/city2.51e0715d.JPG"
+  },
+  {
+    "revision": "adf250117ac315ec8cba5c89f12e3186",
+    "url": "/portfolio/static/media/city3.adf25011.JPG"
+  },
+  {
+    "revision": "b8527d7e0e5d129ae1907a86ca6502bd",
+    "url": "/portfolio/static/media/city4.b8527d7e.JPG"
+  },
+  {
+    "revision": "d4e92ffea1025c56fe8661a56141f875",
+    "url": "/portfolio/static/media/city5.d4e92ffe.JPG"
+  },
+  {
+    "revision": "1d336c85de6170888c7948c7f463b83b",
+    "url": "/portfolio/static/media/city6.1d336c85.JPG"
+  },
+  {
+    "revision": "37b67e0295ba9efcfa42558c1555ea38",
+    "url": "/portfolio/static/media/favicon2.37b67e02.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3fc96bc45b084ed76784ca0f23472f60",
+    "url": "/portfolio/static/media/marinelife1.3fc96bc4.JPG"
+  },
+  {
+    "revision": "b92c6c4d32ceac2e8a8c70d4abe2aabf",
+    "url": "/portfolio/static/media/marinelife2.b92c6c4d.JPG"
+  },
+  {
+    "revision": "f4a72f49969ef10dc34715cf0e29bfa0",
+    "url": "/portfolio/static/media/marinelife3.f4a72f49.JPG"
+  },
+  {
+    "revision": "5a4d9452394b4878fc77dbbee3742739",
+    "url": "/portfolio/static/media/marinelife4.5a4d9452.JPG"
+  },
+  {
+    "revision": "6ab3fd26a500dc66fc3bed08d258c0e2",
+    "url": "/portfolio/static/media/marinelife5.6ab3fd26.JPG"
+  },
+  {
+    "revision": "a87009c46811e5cdbe1fdf71ebd6e4c0",
+    "url": "/portfolio/static/media/marinelife6.a87009c4.JPG"
+  },
+  {
+    "revision": "0fe962fec9b756183375667a848d961e",
+    "url": "/portfolio/static/media/nature1.0fe962fe.JPG"
+  },
+  {
+    "revision": "b0eaef5f70f6ab820974739968b3bf0a",
+    "url": "/portfolio/static/media/nature2.b0eaef5f.JPG"
+  },
+  {
+    "revision": "b891827c01f9e0ac25f3bf736a9e523e",
+    "url": "/portfolio/static/media/nature3.b891827c.JPG"
+  },
+  {
+    "revision": "fbd20358305111bbf305226dbd368139",
+    "url": "/portfolio/static/media/nature4.fbd20358.JPG"
+  },
+  {
+    "revision": "09d3d46113e8f350f010eb225de56e08",
+    "url": "/portfolio/static/media/nature5.09d3d461.JPG"
+  },
+  {
+    "revision": "f0fcd7c4bbade2302813a671c2e2101f",
+    "url": "/portfolio/static/media/nature6.f0fcd7c4.JPG"
+  },
+  {
+    "revision": "cec517056ccb674e8eab325edc871b5d",
+    "url": "/portfolio/static/media/pattern1.cec51705.JPG"
+  },
+  {
+    "revision": "927e29490789430c270ba9336e2a90a4",
+    "url": "/portfolio/static/media/pattern2.927e2949.JPG"
+  },
+  {
+    "revision": "531b40c13ee2ea99e4c200ef8a8a6070",
+    "url": "/portfolio/static/media/pattern3.531b40c1.JPG"
+  },
+  {
+    "revision": "e054603f1e38cc8b3e791c45e9a1d4bd",
+    "url": "/portfolio/static/media/pattern4.e054603f.JPG"
+  },
+  {
+    "revision": "65a1f0ff9184d4c9098a9bd6e4f84f02",
+    "url": "/portfolio/static/media/pattern5.65a1f0ff.JPG"
+  },
+  {
+    "revision": "e8591be0e9b66fdbcf3b6cb79290db7b",
+    "url": "/portfolio/static/media/pattern6.e8591be0.JPG"
+  },
+  {
+    "revision": "3b134bd546dfb3f2a95bd548860d5772",
+    "url": "/portfolio/static/media/photography.3b134bd5.jpg"
+  },
+  {
+    "revision": "614567e567ad589b99adb7b2b775cad9",
+    "url": "/portfolio/static/media/plant1.614567e5.JPG"
+  },
+  {
+    "revision": "fd1946d3cd4fcacdfebd8fa7b5d7976c",
+    "url": "/portfolio/static/media/plant2.fd1946d3.JPG"
+  },
+  {
+    "revision": "458e7ecb883f0905dfdca19d83f97019",
+    "url": "/portfolio/static/media/plant3.458e7ecb.JPG"
+  },
+  {
+    "revision": "727d9859e42299e2fb88f7be18745cb5",
+    "url": "/portfolio/static/media/plant4.727d9859.JPG"
+  },
+  {
+    "revision": "889bdbac0d63f28977ce3cdf6f253b4b",
+    "url": "/portfolio/static/media/plant5.889bdbac.JPG"
+  },
+  {
+    "revision": "4d48cfbbe722e8eef3b6cd87ed79a60e",
+    "url": "/portfolio/static/media/plant6.4d48cfbb.JPG"
+  },
+  {
+    "revision": "7629ac4ebd1a558ffded6d83a201915f",
+    "url": "/portfolio/static/media/profile_pic2.7629ac4e.jpg"
+  }
+]);
